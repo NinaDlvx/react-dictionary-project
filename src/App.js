9f8fd9298451e4edccf9c616f6c6a7932429b413
NinaDlvx <165060963+NinaDlvx@users.noreply.github.com>
2024-08-09 09:50:31 +0200

    Made working search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-        <header className="App-header"> Hello World</header>
+        <header className="App-header">My React Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Nina Delvaux</footer>
       </div>
     </div>
   );
