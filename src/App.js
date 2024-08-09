b756837f24c9d2d44c04ec45d527f205f75d42d4
NinaDlvx <165060963+NinaDlvx@users.noreply.github.com>
2024-08-09 09:18:39 +0200

    Created React App & cleaned unnecessary code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <header className="App-header"> Hello World</header>
+      </div>
+    </div>
+  );
+}
